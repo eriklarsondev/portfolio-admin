82eaef5417ff605cde9544878efcee888bbee40c
Erik Larson <eriklarsonco@gmail.com>
2024-06-29 18:29:41 -0400

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Devfolio (Child)
+
+A scalable headless WordPress theme designed for managing your portfolio content
---
+++ config/index.php
@@ -0,0 +1,12 @@
+<?php
+namespace devfolio;
+
+/**
+ * include theme config
+ */
+include_once dirname(get_template_directory()) . '/classes/index.php';
+
+/**
+ * append additional theme config below
+ */
+$devfolio = new Devfolio();
