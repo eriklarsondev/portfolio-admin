9421b18316810b4ab39e64771232503ba8a2da72
Erik Larson <eriklarsonco@gmail.com>
2025-02-27 20:56:58 -0500

    added nodemailer support

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,20 @@
 module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST'),
+        port: env('SMTP_PORT'),
+        auth: {
+          user: env('SMTP_USER'),
+          pass: env('SMTP_PASS')
+        }
+      },
+      settings: {
+        defaultFrom: `Erik Larson <${env('SMTP_USER')}>`
+      }
+    }
+  },
   upload: {
     config: {
       provider: 'cloudinary',
